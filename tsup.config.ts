564f2f06c873f5f17ba46e0ae1dc7890103c1fd6
ekyza <ekyza.dev@gmail.com>
2025-06-19 17:32:20 +0700

    update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
   outDir: "dist",
   target: "node20.11",
   format: ["esm"],
-  sourcemap: true,
-  clean: true,
   dts: true,
   tsconfig: "tsconfig.json",
+  minify: true,
+  external: ["express", "pg"],
 });
